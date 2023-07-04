5d45c19f3be62ad7d849df89dc1c321dfd1388e1
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-07-04 12:35:04 -0300

    Expose and logs

--- first_script.bat
+++ first_script.bat
@@ -16,4 +16,16 @@ docker rm proxy
 @REM STOP + REMOVE CONTAINER
 docker rm -f proxy
 
+@REM PUBLISH/EXPOSE PORT (PUBLIC PORT):(PRIVATE PORT)
+docker run -d --name proxy -p 8080:80 nginx
+
+@REM SEE LOGS
+docker logs proxy
+
+@REM FOLLOW LOGS
+docker logs -f proxy
+
+@REM LAST 10 logs LOGS
+docker logs --tail 10 -f proxy
+
 pause
