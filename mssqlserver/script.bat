4897ea0266230dd8b938e71a08954a7aeb2835e9
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-07-24 18:39:05 -0300

    Added persistance

--- mssqlserver/script.bat
+++ mssqlserver/script.bat
@@ -2,6 +2,6 @@ docker-compose build
 
 docker-compose up -d
 
-sleep 30
+[ ! -d "./sql_data" ] && sleep 30
 
-docker-compose exec db /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Hola@1234 -i /usr/src/restore.sql
+[ ! -d "./sql_data" ] && docker-compose exec db /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Hola@1234 -i /usr/src/restore.sql
